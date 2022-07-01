e5f32bdd525a4b941b8f2fd5205bf88667c19fe6
ck-kor <ckdrb48@naver.com>
2022-07-01 21:44:08 +0900

    댓글 modifiedAt 추가

--- src/main/java/com/tutti/backend/dto/comment/CommentRequestDto.java
+++ src/main/java/com/tutti/backend/dto/comment/CommentRequestDto.java
@@ -2,8 +2,11 @@ package com.tutti.backend.dto.comment;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 // 코멘트 작성 request
 @Getter
 public class CommentRequestDto {
     private String comment;
+    private String  modifiedAt;
 }
