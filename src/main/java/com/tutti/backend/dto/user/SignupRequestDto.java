d0dbf5b0749f3e19387cd16cbdcf2d616b5b35a4
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2022-07-08 10:30:39 +0900

    genreSelected 추가
    genreSelected 추가

--- src/main/java/com/tutti/backend/dto/user/SignupRequestDto.java
+++ src/main/java/com/tutti/backend/dto/user/SignupRequestDto.java
@@ -30,5 +30,7 @@ public class SignupRequestDto {
 
     String youtubeUrl;
 
+    boolean[] genreSelected;
+
 }
 
--- src/main/java/com/tutti/backend/dto/user/request/UserUpdateRequestDto.java
+++ src/main/java/com/tutti/backend/dto/user/request/UserUpdateRequestDto.java
@@ -14,5 +14,7 @@ public class UserUpdateRequestDto {
 
     String[] genre;
 
+    boolean[] genreSelected;
+
 
 }
