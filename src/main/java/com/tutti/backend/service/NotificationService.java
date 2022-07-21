7f85becc1e47fdcee96a33e73bdf81acb5e34077
minji <alswlwkd20@naver.com>
2022-07-21 11:24:00 +0900

    timeout 시간 변경

--- src/main/java/com/tutti/backend/service/NotificationService.java
+++ src/main/java/com/tutti/backend/service/NotificationService.java
@@ -8,6 +8,7 @@ import com.tutti.backend.dto.Notification.NotificationDetailsDto;
 import com.tutti.backend.repository.EmitterRepository;
 import com.tutti.backend.repository.NotificationRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import java.io.IOException;
 import java.util.Map;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class NotificationService {
@@ -62,6 +64,7 @@ public class NotificationService {
                     .data(data));
         }catch (IOException exception){
             emitterRepository.deleteById(eventId);
+            log.error("연결오류",exception);
             throw new RuntimeException("연결 오류");
         }
     }
